6dcff320f3c24e3977c0e90a7f8c59251b4beef8
Elke Burgers <elke.burgers@hotmail.com>
2019-11-22 12:10:36 -0300

    Ffunção request, envio de informacoes com formulario, e token para criptografia do servidor.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,4 +26,34 @@ class HomeController extends Controller
        return view('home', ["listaUsuarios"=>$listaUsuarios]);
        // colocando a lista de usuarios - array - na view para exibir na home
     }
+
+    // aula 22.11 - passo 2
+    // primeiro request é nome da rota, segundo Request com maiuscula eh a classe, e vou receber o parametro da funcao que eh um objeto, o terceiro request
+    public function request(Request $request, $id=0){
+      //  dd($request);
+      // dd substitui o var-dump + exit
+      if($id==0){
+         echo "Passe um parametro.";
+         // if para não retornar um erro laravel na tela quando o usuario nao informar o paramentro do id
+         }else{
+      dd($id);
+         }
+      //para retornar o id solicitado na rota. Pode ser numero ou string, ele retorna na tela o que escrevemos na url.
+      }        
+
+      //o ideal seria criar um controller para cada view, estamos usando o mesmo porque sao apenas testes
+      public function exibirFormulario(){
+         return view('formulario');
+      }
+
+      // para retornar as informacoes do formulario, usa a classe e o objeto request
+      // todos os metodos post de formulario sao criptografadas pelo laravel, gerando um token, que o servidor exige receber junto com a cripto key para liberar os dados na tela - para isso, usamos a tag @csrf no blade
+      public function cadastrarFormulario(Request $request){
+         dd($request->nome);
+         // ->nome para a tela retornar o dado cadastrado no campo nome do formulario  (acessar o dado, sem precisar de array associativo)
+         // se quiser exbir mais de um parametro, ao inves do dd, que tem exit, usar varios echo.
+         // echo ($request->nome);
+         // echo ($request->idade);
+      }
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,12 @@ Route::get('/home', 'HomeController@viewHome');
 
 Route::get('/cidade', 'CidadeController@viewCidade');
 
+// aula 22.11.2019 - passo 1
+// primeiro o que o ususario digita, depois a pagina para que vai, e o arroba indica o metodo (que criamos) para chegar na pagina que queremos
+//depois vai para a pasta controller
+Route::get('/request/id/{id?}','HomeController@request');
+// para receber id na url, colocamos o id. O id no bigode esta informando que vai receber uma variavel na url que eh o id que estamos colocando apos o request.
+
+Route::get('/formulario','HomeController@exibirFormulario');
+Route::post('/formulario','HomeController@cadastrarFormulario');
+
